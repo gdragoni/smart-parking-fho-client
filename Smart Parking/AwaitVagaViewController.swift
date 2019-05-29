4036ce2362b54856092809f134dd976c0c12b82b
Gabriel Dragoni <ga.dragoni@gmail.com>
2019-05-28 21:32:52 -0300

    Tela para ir até a vaga

---
+++ Smart Parking/AwaitVagaViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AwaitVagaViewController.swift
+//  Smart Parking
+//
+//  Created by Gabriel Dragoni on 28/05/19.
+//  Copyright © 2019 Dragoni. All rights reserved.
+//
+
+import Foundation
---
+++ Smart Parking/Vaga.swift
@@ -0,0 +1,9 @@
+//
+//  Vaga.swift
+//  Smart Parking
+//
+//  Created by Gabriel Dragoni on 24/05/19.
+//  Copyright © 2019 Dragoni. All rights reserved.
+//
+
+import Foundation
